62fd44ad07bbd641677c205e3b133a3f3fc5df6f
CoffeeCrashedCoder <coffeecrashedcoder@gmail.com>
2026-09-02 14:36:53 +0200

    glfw test - runs

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include "Window.h"
+#include <thread>
+#include <chrono>
 
 int main()
 {
+    int i = 0;
+
     try
     {
         Window window(1920, 1080, "DeluluEngine");
@@ -13,9 +17,13 @@ int main()
 
             glClearColor(0.1f, 0.1f, 0.15f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT);
-
-
             window.SwapBuffers();
+             while(i != 5)
+            {
+                std::this_thread::sleep_for(std::chrono::seconds(1));
+                i++;
+                std::cout << i << std::endl;
+            } 
         }
     }
     catch (const std::exception& e)
